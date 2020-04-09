a144b7198f86acc657b635a794a813ef446aba5f
Tim Kieltyka <tk848615@ohio.edu>
2020-04-09 15:53:15 -0400

    push 1

---
+++ MIS4200_Team11B/DAL/MIS4200_Team11Bcontext.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using MIS4200_Team11B.Models;
+using System.Linq;
+using System.Web;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace MIS4200_Team11B.DAL
+{
+    public class MIS4200_Team11Bcontext : DbContext // inherits from DbContext
+    {
+        
+            public MIS4200_Team11Bcontext() : base("name=DefaultConnection")
+            {
+                // this method is a 'constructor' and is called when a new context is created
+                // the base attribute says which connection string to use
+            }
+            // Include each object here. The value inside <> is the name of the class,
+            // the value outside should generally be the plural of the class name
+            // and is the name used to reference the entity in code
+            public DbSet<UserDetails> UserDetails { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<OneToManyCascadeDeleteConvention>();
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}
---
+++ MIS4200_Team11B/Models/UserDetails.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace MIS4200_Team11B.Models
+{
+    public class UserDetails
+    {
+        [Required]
+        public Guid ID { get; set; }
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+        [Required]
+        [Display(Name = "First Name")]
+        public string firstName { get; set; }
+        [Required]
+        [Display(Name = "Last Name")]
+        public string lastName { get; set; }
+        [Display(Name = "Primary Phone")]
+        [Phone]
+        public string PhoneNumber { get; set; }
+        [Display(Name = "Office")]
+        public string Office { get; set; }
+        [Display(Name = "Current position")]
+        public string Position { get; set; }
+
+        [Display(Name = "Hire Date")]
+        [DataType(DataType.Date)]
+        public DateTime hireDate { get; set; }
+
+
+        [Display(Name = "Profile Picture")]
+        [DataType(DataType.ImageUrl)]
+        public string photo { get; set; }
+
+
+        public string employeeName
+        {
+            get
+            {
+                return lastName + ", " + firstName;
+            }
+
+        }
+    }
+}
